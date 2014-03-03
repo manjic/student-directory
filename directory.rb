f10fb3bc24599754e92b05d0232040d998011d2a
Manjit Singh <manjitsingh23@gmail.com>
2014-03-03 00:27:21 +0000

    changed puts command to print command

--- directory.rb
+++ directory.rb
@@ -1,48 +1,22 @@
-students = [
- [:name => "Mario Gintili",  :cohort => :february],
- {:name => "Mikhail Dubov",  :cohort => :february},
- {:name => "Karolis Noreika", :cohort =>:february},
- {:name => "Michael Sidon",   :cohort => :february},
- {:name => "Charles De Barros", :cohort => :february},
- {:name => "Ruslan Vikhor",  :cohort => :february},
- {:name => "Toby Retallick",  :cohort =>:february},
- {:name => "Mark Mekhaiel",  :cohort =>:february},
- {:name => "Sarah Young",  :cohort => :february},
- {:name => "Hannah Wight",  :cohort => :february},
- {:name => "Khushkaran Singh",  :cohort=> :february},
- {:name => "Rick brunstedt",  :cohort => :february},
- {:name => "Manjit Singh",  :cohort => :february},
- {:name => "Alex Gaudiosi",   :cohort => :february},
- {:name => "Ross Hepburn",   :cohort => :february},
- {:name => "Natascia Marchese",  :cohort => :february},
- {:name => "Tiffanie Chia",  :cohort => :february},
- {:name => "Matthew Thomas",   :cohort => :february},
- {:name => "Freddy McGroarty",  :cohort => :february},
- {:name => "Tyler Rollins",   :cohort =>:february},
- {:name => "Richard Curteis",   :cohort => :february},
- {:name => "Anna Yanova",  :cohort => :february},
- {:name => "Andrew Cumine",  :cohort => :february}
-] 
-
-def puts_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+def print_header
+  print "The students of my cohort at Makers Academy \n"
+  print "------------- \n"
 end
 
-def puts_students(students)
+def print_students(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    print "#{student[:name]} (#{student[:cohort]} cohort) \n"
   end
 end
 
-def puts_footer(students)
-  puts "Overall, we have #{students.length} great students"
+def print_footer(students)
+  print "Overall, we have #{students.length} great students \n"
 end
 
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  print "Please enter the names of the students \n"
+  print "To finish, just hit return twice \n"
   # create an empty array
   students = []
   # get the first name
@@ -51,7 +25,7 @@ def input_students
   while !name.empty? do
     # add the student hash to the array
     students << {:name => name, :cohort => :february}
-    puts "Now we have #{students.length} students"
+    print "Now we have #{students.length} students \n"
     # get amother name from the user
     name = gets.chomp
   end
@@ -60,6 +34,6 @@ def input_students
 end
 
 students = input_students
-puts_header
-puts_students(students)
-puts_footer(students)
+print_header
+print_students(students)
+print_footer(students)
