31804c744c9aff2bafa6d0f5a68cfc6ff45ae465
Manjit Singh <manjitsingh23@gmail.com>
2014-03-03 00:50:31 +0000

    Added hobby and height questions

--- directory.rb
+++ directory.rb
@@ -7,7 +7,7 @@ def print_students(students)
   i = 0
   while i < students.length
     student = students[i]
-    print "#{student[:name]} (#{student[:cohort]} cohort) \n" 
+    print "#{student[:name]} (#{student[:cohort]} cohort) #{student[:hobbies]} #{student[:height]} \n" 
     i += 1
   end
 end
@@ -16,27 +16,37 @@ def print_footer(students)
   print "Overall, we have #{students.length} great students \n"
 end
 
-
 def input_students
   print "Please enter the names of the students \n"
   print "To finish, just hit return twice \n"
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
-  #while name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {:name => name, :cohort => :february}
-    print "Now we have #{students.length} students \n"
-    # get amother name from the user
     name = gets.chomp
-  end
+    puts "Tell us a hobby"
+    hobbies = gets.chomp
+    puts "How tall are you?"
+    height = gets.chomp
+      while !name.empty? do
+       # add the student hash to the array
+       students << {:name => name, :cohort => :february, :hobbies=> hobbies, :height => height}
+       print "Now we have #{students.length} students \n"
+       # get amother name from the user
+       name = gets.chomp
+        if name.empty?
+          puts "Thanks for the info"
+        else   
+          puts "Tell us a hobby"
+          hobbies = gets.chomp
+          puts "How tall are you?"
+          height = gets.chomp
+        end
+      end
   #return the array of students
   students
 end
 
+
 students = input_students
 print_header
-print_students(students)
+# print_students(students)
 print_footer(students)
